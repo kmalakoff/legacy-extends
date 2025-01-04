cd199bd98bf31596f2df9e460fef03f180dcda54
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:19:47 -0800

    Upgrade modules

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,17 @@
-/* eslint-disable */
-
 function ensureProperties(names, self, args) {
   if (self[names[0]] !== args[0]) {
-    var length = names.length;
-    for (var index = 0; index < length; index++) {
+    const length = names.length;
+    for (let index = 0; index < length; index++) {
       self[names[index]] = args[index];
     }
   }
 }
 
-var hasProp = {}.hasOwnProperty;
+const hasProp = {}.hasOwnProperty;
 function extendLegacy(child, parent, options) {
-  var initialize = options && options.ensureProperties && options.ensureProperties.length ? ensureProperties.bind(null, options.ensureProperties) : null;
+  const initialize = options && options.ensureProperties && options.ensureProperties.length ? ensureProperties.bind(null, options.ensureProperties) : null;
 
-  for (var key in parent) {
+  for (const key in parent) {
     if (hasProp.call(parent, key)) child[key] = parent[key];
   }
   function ctor() {}
@@ -23,24 +21,30 @@ function extendLegacy(child, parent, options) {
 
   child.super_ = parent.prototype;
   child.superConstruct = function construct() {
+    // biome-ignore lint/style/noArguments: <explanation>
     parent.prototype.constructor.apply(this, arguments);
+
+    // biome-ignore lint/style/noArguments: <explanation>
     !initialize || initialize(this, arguments);
     return this;
   };
 }
 
 function extendReflect(child, parent, options) {
-  var initialize = options && options.ensureProperties && options.ensureProperties.length ? ensureProperties.bind(null, options.ensureProperties) : null;
+  const initialize = options && options.ensureProperties && options.ensureProperties.length ? ensureProperties.bind(null, options.ensureProperties) : null;
 
   Reflect.setPrototypeOf(child.prototype, parent.prototype);
   Reflect.setPrototypeOf(child, parent);
 
   child.super_ = parent.prototype;
   child.superConstruct = function construct() {
-    var self = Reflect.construct(parent, arguments, child);
+    // biome-ignore lint/style/noArguments: <explanation>
+    const self = Reflect.construct(parent, arguments, child);
+
+    // biome-ignore lint/style/noArguments: <explanation>
     !initialize || initialize(self, arguments);
     return self;
   };
 }
 
-module.exports = typeof Reflect === 'undefined' ? extendLegacy : extendReflect;
+export default typeof Reflect === 'undefined' ? extendLegacy : extendReflect;
--- test/spec/platform-specific.test.js
+++ test/spec/platform-specific.test.js
@@ -1,5 +1,5 @@
-var compare = require('semver-compare');
+const compare = require('semver-compare');
 
-const HAS_CLASS = compare(process.versions.node, '6.0.0') >= 0;
+const HAS_CLASS = compare(process.version, '6.0.0') >= 0;
 
 !HAS_CLASS || require('./es6');
